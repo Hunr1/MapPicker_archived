001baec7105a2ccf92746b02c05133104c6a1eee
Henri <henri.laaja@gmail.com>
2019-11-09 14:48:43 +0200

    Lisätty tiedostot: HaettuTurnaus.java, haettu_turnaus.xml LisaaOttelu.java, lisaa_ootelu.xml LuoUusiTurnaus.java, luo_uusi_turnaus.xml TurnauksenHallinta.java, turnauksen_hallinta.xml
    Lisätty luokkien tiedot AndroidManifest.xml tiedostoon

--- app/src/main/java/ryhma4/mappicker/TurnauksenHallinta.java
+++ app/src/main/java/ryhma4/mappicker/TurnauksenHallinta.java
@@ -3,11 +3,11 @@ package ryhma4.mappicker;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TurnauksenHallinta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.turnauksen_hallinta);
     }
 }
