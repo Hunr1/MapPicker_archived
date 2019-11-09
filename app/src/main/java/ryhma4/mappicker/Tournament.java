6af139600a77a40336169a41853d37d5a2210735
Antti <antti.rajala@luukku.com>
2019-11-09 16:40:31 +0200

    Turnausten hallintaan tarvittavat luokat (Tournament, TournamentApplication ja TournamentEngine) luotu

---
+++ app/src/main/java/ryhma4/mappicker/Tournament.java
@@ -0,0 +1,14 @@
+package ryhma4.mappicker;
+
+public class Tournament {
+
+    private String tournamentID;
+
+    public String getTournamentID() {
+        return tournamentID;
+    }
+
+    public void setTournamentID(String tournamentID) {
+        this.tournamentID = tournamentID;
+    }
+}
