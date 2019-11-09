9136c8d23abd835e0f80db622142204ab4d09b19
Antti <antti.rajala@luukku.com>
2019-11-09 16:43:42 +0200

    TournamentEngineä siistitty

--- app/src/main/java/ryhma4/mappicker/TournamentEngine.java
+++ app/src/main/java/ryhma4/mappicker/TournamentEngine.java
@@ -3,25 +3,22 @@ import java.util.ArrayList;
 
 public class TournamentEngine {
 
-    public class ManagerEngine {
+    private ArrayList<Tournament> allTournaments = new ArrayList<>();
 
-        private ArrayList<Tournament> allTournaments = new ArrayList<>();
+    public void addTournament(Tournament person) {
+        allTournaments.add(person);
+    }
 
-        public void addTournament(Tournament person) {
-            allTournaments.add(person);
+    public int countOfTournaments() {
+        return allTournaments.size();
+    }
+
+    public Tournament tournamentByID(int index) {
+        if (allTournaments.size() > index) {
+            return allTournaments.get(index);
         }
-
-        public int countOfTournaments() {
-            return allTournaments.size();
-        }
-
-        public Tournament tournamentByID(int index) {
-            if (allTournaments.size() > index) {
-                return allTournaments.get(index);
-            }
-            else {
-                return null;
-            }
+        else {
+            return null;
         }
     }
 }
