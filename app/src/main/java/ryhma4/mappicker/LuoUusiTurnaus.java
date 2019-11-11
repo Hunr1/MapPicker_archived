f0f977f727f51d4b3f880d9b8cff707b0b858ebf
juhsf <t7raju00@students.oamk.fi>
2019-11-11 15:34:08 +0200

    luouusiturnaus.java ja -.xml jatkettu

--- app/src/main/java/ryhma4/mappicker/LuoUusiTurnaus.java
+++ app/src/main/java/ryhma4/mappicker/LuoUusiTurnaus.java
@@ -1,13 +1,33 @@
 package ryhma4.mappicker;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class LuoUusiTurnaus extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LuoUusiTurnaus extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCancel;
+    private Button btnGenerate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.luo_uusi_turnaus);
+
+        btnCancel = findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.btnCancel) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
